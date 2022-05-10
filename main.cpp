26db3d23ef9663c9305dd26682bc578b28eec53f
Chandler Kramer <ck62013@gmail.com>
2022-05-10 18:40:23 -0500

    Adding for loop

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream> 
+#include <string>
+#include <cstdio> 
+
+
+
 using namespace std; 
 
+
+// Adding a comment for funsies
+
+// Global Variable
+int a = 10;
+// Showing String Creation 
+
+
+
+
+// Function Prototype 
+int second_fun(int x); 
+
+// Main Function 
 int main(){
     cout<<"Hello World\n"; 
+    cout<<a<<endl; 
+    for(int i = 0; i<10; i++){
+    second_fun(i); 
+
+    }
+    return 0; 
+}
+
+// Second Function to show global variable usage 
+int second_fun(int x){
+    printf("String formatting %i\n", x); 
     return 0; 
 }
