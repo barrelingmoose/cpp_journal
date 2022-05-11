88dd9d599ccd18583621c3486010136de04bbe95
Chandler Kramer <ck62013@gmail.com>
2022-05-10 20:56:57 -0500

    Adding recursion example

---
+++ src/recursion.cpp
@@ -0,0 +1,27 @@
+// Factorial of n = 1*2*3*...*n
+// Example Code courtesy of Programiz.com
+// Link: https://www.programiz.com/cpp-programming/recursion 
+
+#include <iostream>
+using namespace std;
+
+int factorial(int);
+
+int main() {
+    int n, result;
+
+    cout << "Enter a non-negative number: ";
+    cin >> n;
+
+    result = factorial(n);
+    cout << "Factorial of " << n << " = " << result<<endl;
+    return 0;
+}
+
+int factorial(int n) {
+    if (n > 1) {
+        return n * factorial(n - 1);
+    } else {
+        return 1;
+    }
+}
